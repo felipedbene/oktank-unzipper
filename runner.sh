5333e3a684cd0d6a512a02015df30458dc851846
De Bene <benfelip@38f9d3c74477.ant.amazon.com>
2020-01-08 11:43:25 -0600

    overwrite

--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #Download the latest version of code
-wget https://raw.githubusercontent.com/felipedbene/oktank-unzipper/master/handler.py
+wget https://raw.githubusercontent.com/felipedbene/oktank-unzipper/master/handler.py -O handler.py
 python3 handler.py
